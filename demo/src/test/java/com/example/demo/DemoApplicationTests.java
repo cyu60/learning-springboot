c89c02d96521f3d59400fc49767bc592845e992d
cyu60 <chinatchinat123@gmail.com>
2021-05-21 12:46:32 +0800

    add simple beans and tests using application ctx

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.NumberFormat;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired private ApplicationContext ctx;
+
+	// Need to specify Qualifier in order to get the correct number format
+	@Qualifier("defaultCurrencyFormat")
+	@Autowired
+	private NumberFormat nf;
+
 	@Test
-	void contextLoads() {
+	public void getDefaultCurrency() {
+		double amount = 123.456;
+		System.out.println(nf.format(amount));
+		return;
+	}
+
+	// get the nf for the german currency from the ctx
+	@Test
+	public void getGermanCurrency() {
+		double amount = 123.456;
+		NumberFormat germanCurrencyFormat = ctx.getBean("germanCurrencyFormat", NumberFormat.class);
+		System.out.println(germanCurrencyFormat.format(amount));
+		return;
+	}
+
+
+	@Test
+	public void contextLoads() {
+		int count = ctx.getBeanDefinitionCount();
+		System.out.println("num of beans: " + count);
+		return;
 	}
 
 }
