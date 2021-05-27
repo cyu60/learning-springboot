2e42515df280a0fe4cc39312e97308165703d79f
cyu60 <chinatchinat123@gmail.com>
2021-05-27 18:13:55 +0800

    added example for my batis

---
+++ mybatis/src/main/java/com/example/mybatis/services/UsersResource.java
@@ -0,0 +1,37 @@
+package com.example.mybatis.services;
+
+import com.example.mybatis.entities.Users;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/users")
+public class UsersResource {
+
+    private UsersMapper usersMapper;
+
+    public UsersResource(UsersMapper usersMapper) {
+        this.usersMapper = usersMapper;
+    }
+
+
+    @GetMapping("/all")
+    public List<Users> getAll() {
+        return usersMapper.findAll();
+    }
+
+    @GetMapping("/update")
+    private List<Users> update() {
+
+        Users users = new Users();
+        users.setName("Youtube");
+        users.setSalary(2333L);
+
+        usersMapper.insert(users);
+
+        return usersMapper.findAll();
+    }
+}
---
+++ persistence/src/main/java/com/example/persistence/dao/JpaOfficerDAO.java
@@ -0,0 +1,2 @@
+package com.example.persistence.dao;public class JpaOfficerDAO {
+}
